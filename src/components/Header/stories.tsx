1e4a25e5d6892344e1bf3c941c196e169288d818
lucaslpa12345 <lucaslpa12345@gmail.com>
2021-08-24 01:02:53 -0300

    remove Header and Main Components

--- src/components/Header/stories.tsx
+++
@@ -1,9 +0,0 @@
-import { Header } from '.'
-
-const stories = {
-  title: 'Header',
-  component: Header,
-}
-export default stories
-
-export const HeaderDefault = () => <Header logoName="Bloguinho" />
